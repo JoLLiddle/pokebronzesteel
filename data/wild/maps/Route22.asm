1ab3d4e021d5f19bf9bad6708b55d0238ff84ec2
JoLLiddle <joliddlemail@gmail.com>
2024-12-25 01:50:52 +1030

    Removed extra raichus from the start of the game

--- data/wild/maps/Route22.asm
+++ data/wild/maps/Route22.asm
@@ -2,9 +2,9 @@ Route22WildMons:
 	def_grass_wildmons 25 ; encounter rate
 	db  3, RATTATA
 IF DEF(_RED)
-	db  3, RAIMIZUCHU
-	db  4, RAIKUSACHU
-	db  4, RAIKAENCHU
+	db  3, NIDORAN_M
+	db  4, RATTATA
+	db  4, NIDORAN_M
 	db  2, RATTATA
 	db  2, NIDORAN_M
 	db  3, SPEAROW
